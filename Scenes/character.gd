4fa09ef62d2f59608f2806659e28c2af706f1dec
ctlnctln <catalinatudor98@gmail.com>
2026-01-31 19:02:57 +0200

    Shop scene and NPC
    Added final background to SHOP scene; Added changing NPC texture

---
+++ Scenes/character.gd
@@ -0,0 +1,13 @@
+extends Control
+#@onready var my_sprite = get_node("sprite")
+#var my_texture = load('res://Assets/Sprites/npc_negru.png')
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+	#my_sprite.texture = my_texture
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
---
+++ Scenes/main_main.gd
@@ -0,0 +1,13 @@
+extends Control
+@onready var npc = $Customer
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	npc.texture = load("res://Assets/Sprites/npc_line.png")
+	await get_tree().create_timer(10).timeout
+	npc.texture = load("res://Assets/Sprites/npc_line_red.png")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
