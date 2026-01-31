c4ce908b814898b0d14d34c28d0f72dea4e74e56
ctlnctln <catalinatudor98@gmail.com>
2026-01-31 19:17:30 +0200

    Added first NPC and changed size

--- Scenes/main_main.gd
+++ Scenes/main_main.gd
@@ -3,9 +3,11 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	npc.texture = load("res://Assets/Sprites/npc_line.png")
-	await get_tree().create_timer(10).timeout
-	npc.texture = load("res://Assets/Sprites/npc_line_red.png")
+	npc.texture = load("res://Assets/Sprites/npc_1.png")
+	
+	#npc.texture = load("res://Assets/Sprites/npc_line.png")
+	#await get_tree().create_timer(10).timeout
+	#npc.texture = load("res://Assets/Sprites/npc_line_red.png")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
