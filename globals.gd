183d48b074b496a86e2c6506986025018d1b1e6e
voicu-bogdan <bogdan.iulian.voicu@gmail.com>
2026-02-01 13:22:56 +0200

    added colors

--- globals.gd
+++ globals.gd
@@ -1,11 +1,14 @@
 extends Node
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+var bloodRed = "#7c0200"
+var darkPurple = "#301934"
+var teal = "#176161"
+var limeGreen = "#32CD32"
+var darkBlue = "#030626"
+var blackMate = "#28282B"
+var strawberry = "#FA5053"
+var sweetGreen = "#B0F5AE"
+var lilacPurple = "#886290"
+var peachyOrange = "#ffcf09"
+var vanillaCream = "#ffb0ea"
+var seafoamBlue = "#9dd9e4"
